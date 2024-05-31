d313420c8268d11a2e0cc2c665d566b05bf65489
mikesmallhelp <mikesmallhelp@gmail.com>
2024-05-31 15:46:05 +0300

    Petterin taulukko

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import { AfterBotti } from "@/components/component/after-botti";
+import { PetterinTaulukko } from "@/components/component/petterin-taulukko";
 
 export default function Home() {
   return (
+    <>
     <AfterBotti/>
+    <PetterinTaulukko/>
+    </>
   );
 }
